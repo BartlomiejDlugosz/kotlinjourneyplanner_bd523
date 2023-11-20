f84581918159dd9a80c22ee1e6d4321900105f6a
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2023-11-20 16:25:49 +0000

    Added map generator

--- src/main/kotlin/journeyplan/RoutePlanner.kt
+++ src/main/kotlin/journeyplan/RoutePlanner.kt
@@ -1,26 +1,76 @@
 package journeyplan
 
 import java.net.URL
+import kotlin.concurrent.thread
 
-private val url =
-    "https://api.tfl.gov.uk/Line/central/Route/Sequence/all?app_id=80a6fb65fbfc4bed889483642b39ca85&app_key=bbe228b0eaa94cc9be87ba869cee5b7d"
-private val centralStations = URL("https://api.tfl.gov.uk/Line/central/StopPoints?app_id=80a6fb65fbfc4bed889483642b39ca85&app_key=bbe228b0eaa94cc9be87ba869cee5b7d").readText()
+val lines: List<String> = listOf(
+  "central",
+  "circle",
+  "district",
+  "northern",
+  "jubilee",
+  "bakerloo",
+  "piccadilly",
+  "metropolitan",
+  "victoria",
+  "hammersmith-city",
+  "elizabeth"
+)
+
 // Add your code for the route planner in this file.
 class SubwayMap(val segments: List<Segment>)
 
-fun lookUpStationCode(stationCode: String): String {
-    return centralStations.substringAfter(stationCode).substringAfter("commonName\":\"").substringBefore("\"")
+fun lookUpStationCode(stationCode: String, stations: String): String {
+  return stations.substringAfter(stationCode).substringAfter("commonName\":\"").substringBefore("\"")
+    .substringBefore(" Underground Station")
+}
+
+fun getStationNames(line: String): String {
+  return URL("https://api.tfl.gov.uk/Line/$line/StopPoints?app_id=80a6fb65fbfc4bed889483642b39ca85&app_key=bbe228b0eaa94cc9be87ba869cee5b7d").readText()
+}
+
+fun getStations(line: String): List<List<String>> {
+  val data =
+    URL("https://api.tfl.gov.uk/Line/$line/Route/Sequence/all?app_id=80a6fb65fbfc4bed889483642b39ca85&app_key=bbe228b0eaa94cc9be87ba869cee5b7d").readText()
+  val orderedStations = data.substringAfter("orderedLineRoutes")
+  return orderedStations.split("},").map { x ->
+    x.substringAfter("naptanIds\":[\"").substringBefore("\"]").split("\",\"")
+  }
 }
 
 fun londonUnderground(): SubwayMap {
-    val data = URL(url).readText()
-    val orderedStations = data.substringAfter("orderedLineRoutes")
-    val stations = orderedStations.substringAfter("naptanIds\":[\"").substringBefore("\"]").split("\",\"")
-    println(stations)
-    val sub = SubwayMap(emptyList())
-    return sub
+  val segments: MutableList<Segment> = mutableListOf()
+
+  val threads: MutableList<Thread> = mutableListOf()
+
+  for (line in lines) {
+    val t = thread {
+      val routes = getStations(line)
+      val centralStations = getStationNames(line)
+      val tempSegment: MutableList<Segment> = mutableListOf()
+
+      for (route in routes) {
+        for (i in 0..<(route.size - 1)) {
+          tempSegment.add(
+            Segment(
+              Station(lookUpStationCode(route[i], centralStations)),
+              Station(lookUpStationCode(route[i + 1], centralStations)),
+              Line(line.capitalize())
+            )
+          )
+
+          segments.addAll(tempSegment.distinct())
+        }
+      }
+    }
+    threads.add(t)
+  }
+  threads.forEach { it.join() }
+  println(segments.distinct())
+  return SubwayMap(segments.distinct())
 }
 
 fun main() {
-    lookUpStationCode("940GZZLUEBY")
+//    lookUpStationCode("940GZZLUEBY")
+  londonUnderground()
 }
--- src/main/kotlin/journeyplan/TravelModel.kt
+++ src/main/kotlin/journeyplan/TravelModel.kt
@@ -1,14 +1,17 @@
 package journeyplan
 
 // Add your code for modelling public transport networks in this file.
-class Station(val name: String) {
+data class Station(val name: String) {
   override fun toString() = name
+  override fun equals(other: Any?): Boolean = (other is Station) && this.name == other.name
 }
 
-class Line(val name: String) {
+data class Line(val name: String) {
   override fun toString() = name
+  override fun equals(other: Any?): Boolean = (other is Line) && this.name == other.name
 }
 
-class Segment(val station1: Station, val station2: Station, val line: Line) {
-  override fun toString() = "$station1 to $station2 on the $line line."
+data class Segment(val station1: Station, val station2: Station, val line: Line) {
+  override fun toString() = "$station1 - $station2 on the $line line."
+  override fun equals(other: Any?): Boolean = (other is Segment) && this.station1 == other.station1 && this.station2 == other.station2 && this.line == other.line
 }
