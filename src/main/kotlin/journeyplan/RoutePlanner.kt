7fcd0d8249251a26c741c2cc85b266c70ae8e787
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2023-11-20 14:40:18 +0000

    Upload

--- src/main/kotlin/journeyplan/RoutePlanner.kt
+++ src/main/kotlin/journeyplan/RoutePlanner.kt
@@ -1,3 +1,26 @@
 package journeyplan
 
+import java.net.URL
+
+private val url =
+    "https://api.tfl.gov.uk/Line/central/Route/Sequence/all?app_id=80a6fb65fbfc4bed889483642b39ca85&app_key=bbe228b0eaa94cc9be87ba869cee5b7d"
+private val centralStations = URL("https://api.tfl.gov.uk/Line/central/StopPoints?app_id=80a6fb65fbfc4bed889483642b39ca85&app_key=bbe228b0eaa94cc9be87ba869cee5b7d").readText()
 // Add your code for the route planner in this file.
+class SubwayMap(val segments: List<Segment>)
+
+fun lookUpStationCode(stationCode: String): String {
+    return centralStations.substringAfter(stationCode).substringAfter("commonName\":\"").substringBefore("\"")
+}
+
+fun londonUnderground(): SubwayMap {
+    val data = URL(url).readText()
+    val orderedStations = data.substringAfter("orderedLineRoutes")
+    val stations = orderedStations.substringAfter("naptanIds\":[\"").substringBefore("\"]").split("\",\"")
+    println(stations)
+    val sub = SubwayMap(emptyList())
+    return sub
+}
+
+fun main() {
+    lookUpStationCode("940GZZLUEBY")
+}
--- src/main/kotlin/journeyplan/TravelModel.kt
+++ src/main/kotlin/journeyplan/TravelModel.kt
@@ -1,3 +1,14 @@
 package journeyplan
 
 // Add your code for modelling public transport networks in this file.
+class Station(val name: String) {
+  override fun toString() = name
+}
+
+class Line(val name: String) {
+  override fun toString() = name
+}
+
+class Segment(val station1: Station, val station2: Station, val line: Line) {
+  override fun toString() = "$station1 to $station2 on the $line line."
+}
