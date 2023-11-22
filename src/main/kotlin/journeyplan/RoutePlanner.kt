d4e980693b4d2a5bb5bdff38b582b65a114da018
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2023-11-22 11:46:28 +0000

    Implemented routes from

--- src/main/kotlin/journeyplan/RoutePlanner.kt
+++ src/main/kotlin/journeyplan/RoutePlanner.kt
@@ -1,17 +1,12 @@
 package journeyplan
 
-import java.lang.Math.toRadians
-import java.net.URL
-import java.util.Collections.newSetFromMap
-import java.util.concurrent.ConcurrentHashMap
-import javax.print.attribute.standard.Destination
 import kotlin.concurrent.thread
-import kotlin.math.*
+import kotlin.system.measureTimeMillis
 
 val lines: List<String> = listOf(
-  "central",
-  "circle",
-//  "district",
+//  "central",
+    "circle",
+    "district",
 //  "northern",
 //  "jubilee",
 //  "bakerloo",
@@ -19,69 +14,94 @@ val lines: List<String> = listOf(
 //  "metropolitan",
   "victoria",
 //  "hammersmith-city",
-//  "elizabeth"
+  "elizabeth"
 )
 
 // Add your code for the route planner in this file.
 class SubwayMap(val segments: List<Segment>) {
+    val hashmap: HashMap<Station, List<Segment>> = hashMapOf()
+    init {
 
-  fun routesFrom(origin: Station, destination: Station): List<Route> {
-    val routes = mutableListOf<Route>()
-    if (origin == destination) {
-      routes.add(Route(listOf()))
-      return routes
+        for (segment in segments) {
+            hashmap[segment.station1] = hashmap[segment.station1]?.plus(segment) ?: listOf(segment)
+        }
+//        println(hashmap)
     }
-    val directSegments = segments.filter{ it.station1 == origin }
-    if (directSegments.isEmpty()) return emptyList()
-    val threads = mutableListOf<Thread>()
-    for (segment in directSegments) {
-      val t = thread {
-        val currentRoutes = routesFromHelper(segment.station2, destination, listOf(origin))
-        if (currentRoutes.isNotEmpty()) routes.addAll(currentRoutes.map { Route(listOf(segment) + it.segments) })
-      }
-      threads.add(t)
-    }
-    threads.forEach{it.join()}
-    return routes
-  }
-  fun routesFromHelper(origin: Station, destination: Station, visitedStations: List<Station> = emptyList()): List<Route> {
-//    if (origin == destination) return emptyList()
+    fun routesFrom(origin: Station, destination: Station): List<Route> {
+        val segmentsWeCanFollow = hashmap[origin] ?: emptyList()
+        val routesToDestination = mutableListOf<Route>()
 
-    var routes = mutableListOf<Route>()
-    if (origin == destination) {
-      // Create a route with a single segment if the origin is the destination
-      routes.add(Route(listOf()))
-      return routes
-    }
-    val directSegments = segments.filter{ it.station1 == origin && it.station2 !in visitedStations }
-    if (directSegments.isEmpty()) return emptyList()
+        if (origin == destination) {
+            routesToDestination.add(Route(listOf()))
+            return routesToDestination
+        }
 
-    for (segment in directSegments) {
-//      val currentRoute = mutableListOf<Segment>(segment)
-//      currentRoute.addAll(findRoute(segment.station2, destination, listOf(origin)))
-//      if (currentRoute.last().station2 == destination) routes.add(Route(currentRoute))
-      val currentRoutes = routesFromHelper(segment.station2, destination, visitedStations.plus(origin))//.map{ Route(listOf(segment) + it.segments) }
-      if (currentRoutes.isNotEmpty()) routes.addAll(currentRoutes.map{ Route(listOf(segment) + it.segments) })
-      routes = routes.distinct().toMutableList()
-      routes = routes.filter{ it.segments.last().station2 == destination }.toMutableList()
-//      routes.addAll(currentRoutes)
+        val threads = mutableListOf<Thread>()
+        for (segment in segmentsWeCanFollow) {
+            val t = thread {
+//                println("WORKING ON THREAD $segment")
+                val currentRoutes =
+                    routesFromHelper(segment.station2, destination, listOf(origin))
+                routesToDestination.addAll(currentRoutes.map { Route(listOf(segment) + it.segments) })
+//                println("FINISHED THREAD ${currentRoutes.map { Route(listOf(segment) + it.segments) }.size} ${segment}")
+            }
+            threads.add(t)
+        }
+
+        threads.forEach { it.join() }
+        return routesToDestination.distinct().filter { it.segments.last().station2 == destination }
     }
 
-    return routes
-  }
+    fun routesFromHelper(
+        origin: Station,
+        destination: Station,
+        visitedStations: List<Station> = listOf(origin)
+    ): List<Route> {
+        val segmentsWeCanFollow = (hashmap[origin] ?: emptyList()).filter { it.station2 !in visitedStations }
+        val routesToDestination = mutableListOf<Route>()
+
+        if (origin == destination) {
+            routesToDestination.add(Route(listOf()))
+            return routesToDestination
+        }
+
+        for (segment in segmentsWeCanFollow) {
+            val currentRoutes = routesFromHelper(segment.station2, destination, (visitedStations + origin).distinct())
+            routesToDestination.addAll(currentRoutes.map { Route(listOf(segment) + it.segments) })
+        }
+
+        return routesToDestination.distinct().filter { it.segments.last().station2 == destination }
+    }
 }
-class Route(val segments: List<Segment>) {
-  override fun toString(): String = segments.toString()
+
+data class Route(val segments: List<Segment>) {
+    override fun toString(): String = segments.toString()
+
+    override fun equals(other: Any?): Boolean = (other is Route) && this.segments == other.segments
 }
 
 fun main() {
+    println(measureTimeMillis {
+        val paths = londonUndergroundCustom().routesFrom(Station("South Kensington"), Station("Victoria"))
+        println(paths.size)
+    })
 
-  val paths = londonUndergroundCustom().routesFrom(Station("South Kensington"), Station("Victoria"))
+    println(measureTimeMillis {
+        val paths = londonUndergroundCustom().routesFromHelper(Station("South Kensington"), Station("Victoria"))
+        println(paths.size)
+    })
+
+//    39974
+//    7149
+//    39974
+//    13861
+
 //  val paths = londonUnderground().routesFrom(Station("Chesham"), Station("Charing Cross"))
 //  val paths = londonUnderground().segments.filter { it.station1 == Station("Charing Cross") || it.station2 == Station("Charing Cross")}
-  for (path in paths) {
-    println(path)
-  }
+
+//    paths.forEach { route ->
+//        println(route)
+//    }
 
 //  val stations = londonUnderground()
 
